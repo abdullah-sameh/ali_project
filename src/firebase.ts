9e6d14a2090621a3fdda2b04dd60151548caef9d
Momenwael21 <waelmomen42@gmail.com>
2023-01-29 21:27:02 +0200

    init init firebase

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0S7o0fLTleJtZgIOfQ_Mx7WU0zh-dFhI",
+  authDomain: "carshop-6bb3d.firebaseapp.com",
+  projectId: "carshop-6bb3d",
+  storageBucket: "carshop-6bb3d.appspot.com",
+  messagingSenderId: "630655296117",
+  appId: "1:630655296117:web:76f05b9ac5e96201414e3c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
