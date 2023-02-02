eb0dc112666912c6bd90c1f6f289a372f5aa0fbb
abdullah-sameh <abdullahmaher@std.mans.edu.eg>
2023-02-02 14:03:06 +0200

    card select

--- src/containers/CarTypePage/CarTypePage.jsx
+++ src/containers/CarTypePage/CarTypePage.jsx
@@ -1,35 +1,49 @@
-import Card from "../../components/Card/Card";
-import NavbarPages from "../../components/NavbarPages/NavbarPages";
-import "./CarTypePage.css";
-import { Link, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getCar } from "../../rtk/slices/carSlice";
+import Card from "../../components/Card/Card"
+import NavbarPages from "../../components/NavbarPages/NavbarPages"
+import "./CarTypePage.css"
+import { Link, useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import { getCar } from "../../rtk/slices/carSlice"
 
 export default function CarTypePage() {
-  const dispatch = useDispatch();
-  const { carId } = useParams();
-  const theCar = useSelector((state) => state.car);
+  const dispatch = useDispatch()
+  const { carId } = useParams()
+  // const theCar = useSelector((state) => state.car);
 
-  // theCar:{
-  // spareParts:[
-  //   {
-  //     name:'',
-  //     madeIn:[
-  //       {
-  //         availableNumber: 0,
-  //         gomlaNumber: 0,
-  //         customerNumber: 0,
-  //         country:''
-  //       },
-  //     ],
-  //   },
-  //  ],
-  // }
+  const theCar = {
+    data: {
+      spareParts: [
+        {
+          name: "القطعة الحلوه",
+          madeIn: [
+            {
+              availableNumber: 50,
+              gomlaPrice: 250,
+              customerPrice: 500,
+              country: "مصري",
+            },
+            {
+              availableNumber: 3,
+              gomlaPrice: 10000,
+              customerPrice: 12000,
+              country: "هندي",
+            },
+            {
+              availableNumber: 10,
+              gomlaPrice: 50,
+              customerPrice: 60,
+              country: "باكستاني",
+            },
+          ],
+        },
+      ],
+    },
+  }
 
   useEffect(() => {
-    dispatch(getCar(carId));
-  }, [carId]);
+    dispatch(getCar(carId))
+  }, [carId])
   return (
     <>
       <NavbarPages name={theCar?.data?.modelName} />
@@ -41,14 +55,14 @@ export default function CarTypePage() {
       </form>
       <div className="container cards">
         {theCar?.data?.spareParts?.map((part, index) => {
-          console.log(part);
+          console.log(part)
           return (
             <Card
               key={index}
               nameItem={part?.name}
               countryMade={part?.madeIn}
             />
-          );
+          )
         })}
       </div>
       <Link className="request-btn" to={"./requestPage"}>
@@ -56,5 +70,5 @@ export default function CarTypePage() {
         <i className="material-icons">shopping_cart</i>
       </Link>
     </>
-  );
+  )
 }
