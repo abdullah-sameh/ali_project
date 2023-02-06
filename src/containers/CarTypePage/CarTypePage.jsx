fab9e156aacf7b5d33f7eaaf542fd04c44d5c062
Momenwael21 <waelmomen42@gmail.com>
2023-02-07 00:06:24 +0200

    fix fix a bug in carParts page

--- src/containers/CarTypePage/CarTypePage.jsx
+++ src/containers/CarTypePage/CarTypePage.jsx
@@ -1,27 +1,53 @@
 import Card from "../../components/Card/Card";
 import NavbarPages from "../../components/NavbarPages/NavbarPages";
 import "./CarTypePage.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo, useState } from "react";
 import { getCarById } from "../../rtk/slices/carIdSlice";
+import { setUser } from "../../rtk/slices/userSlice";
+import { getAllCars } from "../../rtk/slices/allCarsSlice";
 
 export default function CarTypePage() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { carId } = useParams();
   const theCar = useSelector((state) => state.carById);
   const [spareParts, setSpareParts] = useState([]);
-  const [searchResult, setSearchResult] = useState([]);
+  const [searchResult, setSearchResult] = useState();
+
+  useEffect(() => {
+    if (!sessionStorage.getItem("user")) {
+      navigate("/");
+    } else {
+      let item = sessionStorage.getItem("user");
+      dispatch(setUser(JSON.parse(item)));
+    }
+    dispatch(getAllCars());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     dispatch(getCarById(carId));
+  }, [carId, dispatch]);
+
+  useEffect(() => {
+    console.log(theCar?.data?.spareParts);
     setSpareParts(theCar?.data?.spareParts);
-  }, [carId, theCar]);
+    setSearchResult([]);
+  }, [theCar]);
 
   const displayParts = useMemo(() => {
-    return searchResult?.map((part, index) => (
-      <Card key={index} nameItem={part?.name} countryMade={part?.madeIn} />
-    ));
+    if (searchResult?.length === 0) {
+      return spareParts?.map((part, index) => (
+        <Card key={index} nameItem={part?.name} countryMade={part?.madeIn} />
+      ));
+    } else {
+      return searchResult?.map((part, index) => (
+        <Card key={index} nameItem={part?.name} countryMade={part?.madeIn} />
+      ));
+    }
   }, [searchResult]);
 
   const searchHandler = (value) => {
